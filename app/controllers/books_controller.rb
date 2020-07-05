f84923d0fd91ec1546a212858260cbf4bd926e09
vagrant <vagrant@localhost.localdomain>
2020-07-05 05:42:38 +0000

    [add]create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,20 @@ class BooksController < ApplicationController
   def show
   end
 
+  def new
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  end
+
   def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
