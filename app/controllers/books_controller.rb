6bd0c9ae4fd4e452bd0810fdbacea4791976ced3
vagrant <vagrant@localhost.localdomain>
2020-07-05 09:08:47 +0000

    [fix]1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,20 +4,17 @@ class BooksController < ApplicationController
 
   def index
   	@books = Book.all
+  	@book = Book.new
   end
 
   def show
   	@book = Book.find(params[:id])
   end
 
-  def new
-  	@book = Book.new
-  end
-
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to books_index_path
+  	redirect_to books_path
   end
 
   def edit
